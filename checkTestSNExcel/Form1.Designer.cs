57f04d02c0b03616df4b3a0047f62823cb2f6ade
Apisit Sirichoke (อภิสิทธิ์ ศิริโชค) <Apisit.Sirichoke@quantaqmb.com>
2022-12-06 14:30:36 +0700

    First commit

---
+++ checkTestSNExcel/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace checkTestSNExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pcbLoad = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtScanSN = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.lbReult = new System.Windows.Forms.Label();
+            this.ofdExcel = new System.Windows.Forms.OpenFileDialog();
+            this.bgwUpload = new System.ComponentModel.BackgroundWorker();
+            this.bgwChkSN = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLoad)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.groupBox1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(416, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(330, 311);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "HOW TO";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pcbLoad);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(3, 162);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(324, 146);
+            this.panel1.TabIndex = 1;
+            // 
+            // pcbLoad
+            // 
+            this.pcbLoad.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pcbLoad.Image = global::checkTestSNExcel.Properties.Resources.Loading;
+            this.pcbLoad.Location = new System.Drawing.Point(0, 0);
+            this.pcbLoad.Name = "pcbLoad";
+            this.pcbLoad.Size = new System.Drawing.Size(324, 146);
+            this.pcbLoad.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pcbLoad.TabIndex = 0;
+            this.pcbLoad.TabStop = false;
+            this.pcbLoad.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(324, 279);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "\r\n1. Click upload excel(In excel, colum has to be removed, leaving only the colum" +
+    " named \"Unit\".)\r\n2. Scan serial number in textbox\r\n3. Reult (OK, SN not found du" +
+    "plicate) \r\nand (NG, SN found duplicate)";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnUpload);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(416, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Upload Excel";
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnUpload.BackgroundImage = global::checkTestSNExcel.Properties.Resources.upload_icon;
+            this.btnUpload.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnUpload.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnUpload.Location = new System.Drawing.Point(6, 34);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(404, 53);
+            this.btnUpload.TabIndex = 0;
+            this.btnUpload.UseVisualStyleBackColor = false;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtScanSN);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox3.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(0, 100);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(416, 100);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Scan Serial number";
+            // 
+            // txtScanSN
+            // 
+            this.txtScanSN.Location = new System.Drawing.Point(6, 43);
+            this.txtScanSN.MaxLength = 23;
+            this.txtScanSN.Name = "txtScanSN";
+            this.txtScanSN.Size = new System.Drawing.Size(404, 33);
+            this.txtScanSN.TabIndex = 1;
+            this.txtScanSN.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtScanSN_KeyDown);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.lbReult);
+            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox4.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox4.Location = new System.Drawing.Point(0, 200);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(416, 111);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Result";
+            // 
+            // lbReult
+            // 
+            this.lbReult.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.lbReult.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbReult.Location = new System.Drawing.Point(6, 29);
+            this.lbReult.Name = "lbReult";
+            this.lbReult.Size = new System.Drawing.Size(404, 72);
+            this.lbReult.TabIndex = 0;
+            this.lbReult.Text = "000000000000000000000000000000000000000000000000000000000000000000000000000000000" +
+    "0\r\n";
+            // 
+            // ofdExcel
+            // 
+            this.ofdExcel.FileName = "ofdExcel";
+            // 
+            // bgwUpload
+            // 
+            this.bgwUpload.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgwUpload_DoWork);
+            this.bgwUpload.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgwUpload_RunWorkerCompleted);
+            // 
+            // bgwChkSN
+            // 
+            this.bgwChkSN.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgwChkSN_DoWork);
+            this.bgwChkSN.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgwChkSN_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(746, 311);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CheckTestSNFromExcel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLoad)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pcbLoad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtScanSN;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label lbReult;
+        private System.Windows.Forms.OpenFileDialog ofdExcel;
+        private System.ComponentModel.BackgroundWorker bgwUpload;
+        private System.ComponentModel.BackgroundWorker bgwChkSN;
+    }
+}
+
---
+++ checkTestSNExcel/Form1.cs
@@ -0,0 +1,276 @@
+﻿using Connect.BLL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace checkTestSNExcel
+{
+    public partial class Form1 : Form
+    {
+        ConnectDB oCon = new ConnectDB();
+        DataTable dt = new DataTable();
+        IOException ex = new IOException();
+        DialogResult result = new DialogResult();
+        int flage = 0, flageChk = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbReult.Text = "";
+        }
+
+        //--Upload Excel file--
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            if (!bgwUpload.IsBusy)
+            {
+                flage = 0;
+                pcbLoad.Visible = true;
+                int size = -1;
+                result = ofdExcel.ShowDialog(); // Show the dialog.
+
+                bgwUpload.RunWorkerAsync();
+            }            
+        }
+
+        /// <summary>
+        /// Provides method to read excel file.
+        /// </summary>
+        /// <param name="filePath">The file path to be read.</param>
+        /// <param name="sheetName">Excel file sheet name for gets data.</param>
+        /// <param name="selectFields">Select fields like * or fields with comma separated.</param>
+        /// <param name="tableName">The name of return DataTable.</param>
+        /// <param name="fileIncludesHeaders">Indicates whether file includes headers.</param>
+        /// <returns>DataTable</returns>
+        public static DataTable ReadExcelFile(String filePath,
+                                          String sheetName,
+                                          String selectFields,
+                                          String tableName,
+                                          Boolean fileIncludesHeaders)
+        {
+            DataSet dataSet = null;
+            DataTable dtReturn = null;
+            string connectionString = string.Empty;
+            string commandText = string.Empty;
+
+            // Indicates the Excel file with header or not.
+            string headerYesNo = string.Empty;
+            string fileExtension = string.Empty;
+            try
+            {
+                if (fileIncludesHeaders == true)
+                {
+                    // Set YES if excel WithHeader is TRUE.
+                    headerYesNo = "YES";
+                }
+                else
+                {
+                    // Set NO if excel WithHeader is FALSE.
+                    headerYesNo = "NO";
+                }
+
+                // Gets file extension for checking.
+                fileExtension = Path.GetExtension(filePath);
+
+                switch (fileExtension.ToUpper())
+                {
+                    case ".XLS":
+
+                        //Take Connection For Microsoft Excel File.
+                        connectionString =
+                          string.Format(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties=""Excel 8.0;IMEX=2.0;HDR={1}""",
+                                        filePath,
+                                        headerYesNo);
+
+                        break;
+
+                    case ".XLSX":
+
+                        //Take Connection For Microsoft Excel File.
+                        connectionString =
+                          string.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=""Excel 12.0;IMEX=2.0;HDR={1}""",
+                                        filePath,
+                                        headerYesNo);
+
+                        break;
+
+                    default:
+
+                        throw new Exception("File is invalid.");
+                }
+
+                commandText = string.Format("SELECT {0} FROM [{1}$]",
+                                            selectFields,
+                                            sheetName);
+
+                dataSet = new DataSet();
+                using (OleDbConnection dbConnection = new OleDbConnection(connectionString))
+                {
+                    OleDbCommand dbCommand = new OleDbCommand(commandText, dbConnection);
+                    dbCommand.CommandType = CommandType.Text;
+                    OleDbDataAdapter dbDataAdapter = new OleDbDataAdapter(dbCommand);
+                    dbDataAdapter.Fill(dataSet);
+                }
+
+                if (dataSet != null &&
+                    dataSet.Tables.Count > 0)
+                {
+                    dataSet.Tables[0].TableName = tableName;
+
+                    // Sets reference of data table.
+                    dtReturn = dataSet.Tables[tableName];
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return dtReturn;
+        }
+
+        private void bgwUpload_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = ofdExcel.FileName;
+                try
+                {
+                    dt = ReadExcelFile(file, "Sheet1", "*", "dt", true);
+                    flage = 0;
+                }
+                catch (IOException ex1)
+                {
+                    ex = ex1;
+                    flage = 1;
+                }
+            }
+        }
+
+        private void bgwUpload_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            pcbLoad.Visible = false;
+            if (flage == 0)
+            {
+                if (dt.Rows.Count > 0)
+                {
+                    //--Show message in label--
+                    lbReult.BackColor = Color.Green;
+                    lbReult.Text = "Upload excel successed.";
+                    playOK();
+                    txtScanSN.Text = "";
+                    txtScanSN.Focus();
+                }
+                else
+                {
+                    //--Show message in label--
+                    lbReult.BackColor = Color.Red;
+                    lbReult.Text = "Upload file not correct or Excel no data or Sheet name != 'Sheet1', Please rename 'Sheet1' in excel file upload.";
+                    playNG();
+                    txtScanSN.Text = "";
+                    txtScanSN.Focus();
+                }
+            }
+            else
+            {
+                lbReult.BackColor = Color.Red;
+                lbReult.Text = ex.ToString().Trim();
+                playNG();
+                txtScanSN.Text = "";
+                txtScanSN.Focus();
+            }
+        }
+
+        private void bgwChkSN_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (dt.Rows.Count > 0)
+            {
+                foreach (DataRow row in dt.Rows)
+                {
+                    if (txtScanSN.Text.Trim() == row["Unit"].ToString().Trim())
+                    {
+                        flageChk = 1;
+                        break;
+                    }
+                }
+            }
+            else //flageChk == -1
+            {
+                flageChk = -1;
+            }
+        }
+
+        private void bgwChkSN_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            pcbLoad.Visible = false;
+            if (flageChk == -1)
+            {
+                //--Show message in label--
+                lbReult.BackColor = Color.Red;
+                lbReult.Text = "No data upload, Please upload file again.";
+                playNG();
+                txtScanSN.Text = "";
+                txtScanSN.Focus();
+            }
+            else if (flageChk == 0)
+            {
+                lbReult.BackColor = Color.Green;
+                lbReult.Text = "OK, Serial number not found duplicate.";
+                playOK();
+                txtScanSN.Text = "";
+                txtScanSN.Focus();
+            }
+            else if (flageChk == 1)
+            {
+                lbReult.BackColor = Color.Red;
+                lbReult.Text = "NG, Serial number found duplicate.";
+                playNG();
+                txtScanSN.Text = "";
+                txtScanSN.Focus();
+            }
+        }
+
+
+        //--Scan SN for check--
+        private void txtScanSN_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && txtScanSN.Text.Length == 23)
+            {
+                if (!bgwChkSN.IsBusy)
+                {
+                    flageChk = 0;
+                    pcbLoad.Visible = true;
+                    bgwChkSN.RunWorkerAsync();
+                }
+            }
+        }
+
+        public void playOK()
+        {
+            string exePath = Application.StartupPath + "\\OK.wav";
+            SoundPlayer simpleSound = new SoundPlayer(exePath);
+            simpleSound.Play();            
+            return;
+        }
+
+        public void playNG()
+        {
+            string exePath = Application.StartupPath + "\\OO.wav";
+            SoundPlayer simpleSound = new SoundPlayer(exePath);
+            simpleSound.Play();            
+            return;
+        }
+    }
+}
